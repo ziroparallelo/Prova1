90d69cf57634637f005abd7e1f457b259328a694
ziroparallelo <ziroparallelo@localhost>
2022-09-03 11:56:16 +0200

    Prova

---
+++ src/main/java/it/polito/tdp/Indovina_Numero/FXMLController.java
@@ -0,0 +1,70 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.Indovina_Numero;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private int segreto;
+	private final int TMAX = 8; //Final sta ad indicare una costante
+	private final int NMAX = 100;
+	private int tentativiFatti;
+	
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnNuovaPartita"
+    private Button btnNuovaPartita; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnProva"
+    private Button btnProva; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtRisultato"
+    private TextArea txtRisultato; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTentativi"
+    private TextField txtTentativi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTentativo"
+    private TextField txtTentativo; // Value injected by FXMLLoader
+
+    @FXML
+    void doNuovaPartita(ActionEvent event) {
+    	
+    	//Gestione di una nuova partita
+    	this.segreto = (int)(Math.random() * NMAX + 1);
+    	this.tentativiFatti = 0;
+    	
+    	//Gestione interfaccia
+    	txtTentativi.setText(Integer.toString(TMAX));
+    	txtRisultato.clear();
+    }
+
+    @FXML
+    void doTentativo(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnNuovaPartita != null : "fx:id=\"btnNuovaPartita\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnProva != null : "fx:id=\"btnProva\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTentativi != null : "fx:id=\"txtTentativi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTentativo != null : "fx:id=\"txtTentativo\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+}
---
+++ src/main/java/it/polito/tdp/Indovina_Numero/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.Indovina_Numero;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
